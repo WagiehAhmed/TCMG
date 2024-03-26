46057c1a308cf99901a6e6b2b7ebe2a80ac93dad
Wagieh Ahmed <wagehahmed1237@gmail.com>
2024-03-26 04:42:32 +0200

    add new domain

--- src/util/serverURL.js
+++ src/util/serverURL.js
@@ -1,4 +1,5 @@
-export const serverUrl = "http://localhost:2000/";
-// export const serverUrl = "https://tcmg-back-end.vercel.app/";
+export const serverUrl = "https://tcmg-back-end.vercel.app/";
 
+// export const serverUrl = "http://localhost:2000/";
+// export const serverUrl = "https://tcmg-back-end.vercel.app/";
 // export const serverUrl = "https://admin.markzk.com/";
