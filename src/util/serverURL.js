c7bd5925d880820bb6259720f07fcd65368d70e3
Wagieh Ahmed <wagehahmed1237@gmail.com>
2024-03-26 23:50:16 +0200

    cloudinary

--- src/util/serverURL.js
+++ src/util/serverURL.js
@@ -1,13 +1,13 @@
 
 //vercel
-export const serverUrl = "https://tcmg-back-end.vercel.app";
-
+// export const serverUrl = "https://tcmg-back-end.vercel.app";
+// 
 //cyclic
 // export const serverUrl = "https://real-gold-sawfish-veil.cyclic.app";
 
 
 //localhost
-// export const serverUrl = "http://localhost:2000";
+export const serverUrl = "http://localhost:2000";
 
 
 
